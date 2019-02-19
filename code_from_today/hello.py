ababc6fc0f79ed9d5eb38eff6b54952017d22551
clbokea <clbo@kea.dk>
2019-02-19 15:13:25 +0100

    png added

--- code_from_today/hello.py
+++ code_from_today/hello.py
@@ -1,8 +1,17 @@
+import sys
+
+def hello(name):
+  if name == 'Alice':
+    name += '?????????'
+  else:
+    call_a_non_xixstin_function() 
+  
+  name += '!!!!!!'
+  print(name)
 
 # Define a main() function that prints a little greeting.
-def main():
-  print('Hello ')
-
+def main(): 
+  hello(sys.argv[1])
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
