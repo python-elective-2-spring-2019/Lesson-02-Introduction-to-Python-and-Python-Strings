de022b01905347f2a598c044124a043ebf964094
clbokea <clbo@kea.dk>
2019-02-19 08:22:16 +0100

    hello

---
+++ code_from_today/hello.py
@@ -0,0 +1,9 @@
+
+# Define a main() function that prints a little greeting.
+def main():
+  print('Hello ')
+
+
+# This is the standard boilerplate that calls the main() function.
+if __name__ == '__main__':
+  main()
